36affeecbf36ff9ff164062664199265395bdbe4
Daniel Price <danprce@gmail.com>
2023-10-24 07:23:48 -0500

    bugs

--- atticus.py
+++ atticus.py
@@ -33,8 +33,6 @@ class PersonalityBot:
 
         openai.api_key = self.api_key_openai
 
-        self.system_message = self.get_system_message()
-
         self.prompt_tokens = 0
         self.tokens_in_function_response = 0
 
@@ -49,17 +47,8 @@ class PersonalityBot:
 
             return {}
 
-    def get_system_message(self):
-
-        message = "You are a chatbot that can search a vector embeddings database with embeddings of the Louisiana Civil Code to answer legal questions for the user. When the user asks you a question about Louisiana law, use the vector search function to search the Civil Code. To aid vector search, take the user's question and rewrite it as a hypothetical answer to increase likelihood of vector search match. The search will return to you the top 10 most similar articles, use those articles to generate a response to the user. If the vector search results do not directly answer the question, ask the user to rephrase their question. Always cite the article number in your response. If the user asks you a question that is not about Louisiana law, you can use the default chatbot function. Always use inline citations in the format 'Explanatory sentence. La. C.C. art. 123' Don't put the article in parentheses." 
-        return message
-
         
     def chat_completion(self, prompt: str, extra_context_from_user: str = ""):
-      
-        self.conversation_history.append({"role": "user", "content": prompt})
-        messages = [{"role": "system", "content": self.system_message}]
-        messages += self.conversation_history
 
         print(f"User's prompt: \n '{prompt}'")
         
@@ -68,12 +57,11 @@ class PersonalityBot:
         reply = ""
         top_10_results = []  # Initialize top_10_results here
                     
-        max_tokens_value = 1000
-
         classifier_completion = openai.ChatCompletion.create(
             model=self.model,
             messages=[{"role": "system", "content": "The user is going to ask you a question about Louisiana law. Decide whether you need more information to answer the question first. If the user just sends a greeting, you need more info. Always assume the question is about Louisiana law unless the user mentions otherwise."},
                         {"role": "user", "content": f"{prompt} | Extra context from user: {extra_context_from_user}"}],
+            temperature=0,
             functions=FUNCTIONS,
             function_call={"name": "classify_question"}
         )
@@ -105,6 +93,7 @@ class PersonalityBot:
                     model=self.model,
                     messages=[{"role": "system", "content": "The user is going to ask you a question about Louisiana law, but you need more information. Ask the user for the specific information you need to answer the question."},
                                 {"role": "user", "content": f"{prompt}"}],
+                    temperature=0,
                 )
 
                 # ...
@@ -121,6 +110,7 @@ class PersonalityBot:
             max_tokens=1000,
             messages=[{"role": "system", "content": "The user is going to ask you a question about Louisiana law. Answer completely."},
                         {"role": "user", "content": "Original question:" + prompt + "Additional context:" + extra_context_from_user}],
+            temperature=0,
         )
 
         raw_response = raw_completion.choices[0].message
@@ -134,6 +124,7 @@ class PersonalityBot:
             functions=FUNCTIONS,
             messages=[{"role": "system", "content": "The user is going to ask you a question about Louisiana law. Use an internet search to try and gather the answer."},
                         {"role": "user", "content": f"{prompt} + {extra_context_from_user} + Louisiana law"}],
+            temperature=0,
             function_call={"name": "search"}
         )
 
@@ -157,7 +148,9 @@ class PersonalityBot:
                 model=self.model,
                 max_tokens=1000,
                 messages=[{"role": "system", "content": "The user is going to ask you a question about Louisiana law. Take the internet serp results and snippets and try to answer the question."},
-                            {"role": "user", "content": f"User Question: {prompt} + {extra_context_from_user} \n Internet Serp Results: \n {function_response}]"}])
+                            {"role": "user", "content": f"User Question: {prompt} + {extra_context_from_user} \n Internet Serp Results: \n {function_response}]"}],                            
+                temperature=0
+                )
 
             summary_response = summary_completion.choices[0].message
 
@@ -166,12 +159,13 @@ class PersonalityBot:
         print("GPT4's summary of internet search results: \n")
         print(internet_reply)
 
-        messages += [{"role": "user", "content": f"A previous GPT4 instance answered the user's question ({prompt} + {extra_context_from_user}) as: \n\n {raw_response}. Another instance summarized the internet's answer to the question as: \n\n {internet_reply}"}]
+        messages = [{"role": "user", "content": f"A previous GPT4 instance answered the user's question ({prompt} + {extra_context_from_user}) as: \n\n {raw_response}. Another instance summarized the internet's answer to the question as: \n\n {internet_reply}"}]
 
         completion = openai.ChatCompletion.create(
             model=self.model,
-            max_tokens=max_tokens_value,
+            max_tokens=1000,
             messages=messages,
+            temperature=0,
             functions=FUNCTIONS,
             function_call={"name": "vector_search_civil_code"},
         )
@@ -191,18 +185,21 @@ class PersonalityBot:
                 
                 # If the function called is vector_search_civil_code, assign the second element of the response to top_10_results
                 if function_name == "vector_search_civil_code":
-                    function_response, top_10_results = function_response
+                    vector_output, top_10_results = function_response
                     
             functions_used.append({"name": function_name, "arguments": function_args})
 
-            messages = [
-                {"role": "system", "content": f"You are a legal answering service. You are going to be given context from GPT4, the internet, and a semantic search of the Louisiana Civil Code. Use the context to answer the user's question: \n {prompt} + {extra_context_from_user} \n. ALWAYS GIVE THE CIVIL CODE ARTICLES THE MOST WEIGHT WHEN ANSWERING THE QUESTION. The raw GPT4 response may include hallucinations. If the user asks you a question that is not about Louisiana law, you can just answer normally. If the answer does not appear to be in the Vector Results you don't have to mention any specific code articles. DO NOT ASSUME THAT JUST BECAUSE YOU ARE GIVEN A CIVIL CODE ARTICLE FOR CONTEXT IT IS RELEVANT TO THE USER'S QUESTION. Always use inline citations in the following format 'Explanatory sentence. La. C.C. art. 123' Don't put the article in parentheses." },
-                {"role": "user", "content": f"Raw ChatPGT Response without help of vector embedding: \n\n {raw_response} \n\n Internet Results: \n\n {internet_reply}  \n\n Vector Results: \n\n {function_response}"},
-            ]
+        messages = [
+            {"role": "system", "content": f"You are Atticus, a legal answering service. You are going to be given context from GPT4, the internet, and a semantic search of the Louisiana Civil Code. Use the context to answer the user's question: \n {prompt} + {extra_context_from_user} \n. ALWAYS GIVE THE CIVIL CODE ARTICLES AND STATUTES THE MOST WEIGHT WHEN ANSWERING THE QUESTION. The raw GPT4 response may include hallucinations. If the user asks you a question that is not about the law, do not answer. If the answer does not appear to be in the vector embeddings results, you don't have to mention any specific code articles. DO NOT ASSUME THAT JUST BECAUSE YOU ARE GIVEN A LAW FOR CONTEXT IT IS RELEVANT TO THE USER'S QUESTION. Always use inline citations in the following format 'Explanatory sentence. La. C.C. art. 123' Don't put your citations in parentheses. Do not cite to the raw GPT4 response." },
+            {"role": "assistant", "content": f"Raw ChatPGT Response without help of vector embedding: \n\n {raw_response} \n\n Internet Results: \n\n {internet_reply}  \n\n Vector Results: \n\n {vector_output}"}
+        ]
+
+        print(f"\n\nFinal Prompt sent to GPT4: \n\n: {messages} \n\n")
 
         final_completion = openai.ChatCompletion.create(
             model=self.model,
             messages=messages,
+            temperature=0,
             )
         
         final_reply = final_completion.choices[0].message['content']
@@ -214,8 +211,6 @@ class PersonalityBot:
             print('\033[92m' + "Function used: " + func_info["name"] + '\033[0m')
             print('\033[92m' + "Function arguments: " + str(func_info["arguments"]) + '\033[0m' + "\n")
 
-        self.conversation_history.append({"role": "assistant", "content": reply})
-        self.system_message = self.get_system_message()
         return final_reply, top_10_results
 
 
